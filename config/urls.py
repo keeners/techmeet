85168c7218bc91942c96aa942c7b6078c27f2f86
Ashia <mail@ashia.ca>
2019-03-16 16:06:21 -0600

    Move settings, urls, and apps. Establish data, domain and interfacet

---
+++ config/urls.py
@@ -0,0 +1,46 @@
+"""Main url routing."""
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.views import defaults as default_views
+
+from rest_framework import routers
+
+from interface.api import views
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
