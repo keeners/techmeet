5b7b9889d85d7a9ded895ae24536489dd3a11724
Bryan Hyshka <bryan@hyshka.com>
2019-03-29 13:33:21 -0600

    Remove app from settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -27,7 +27,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["rest_framework", "webpack_loader"]
+THIRD_PARTY_APPS = ["rest_framework"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS
 
