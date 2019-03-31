e6f7f20960b741d5f2c03dc0db15412842f0fae7
Bryan Hyshka <bryan@hyshka.com>
2019-03-31 15:34:31 -0600

    Linting

--- techmeet/data/templatetags/webpack_check.py
+++ techmeet/data/templatetags/webpack_check.py
@@ -3,11 +3,12 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def is_from_webpack(request) -> bool:
     """Check if the request was forwarded from webpack."""
-    from_webpack = request.META.get('HTTP_X_FROM_WEBPACK', None)
-    if from_webpack == 'true':
+    from_webpack = request.META.get("HTTP_X_FROM_WEBPACK", None)
+    if from_webpack == "true":
         return True
     else:
         return False
