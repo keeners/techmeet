8e0af611c5a14591086fa3ca372cb82d65b7e4a6
Ashia <mail@ashia.ca>
2019-06-15 15:58:34 -0600

    Load the actual custom user
    Fixes #25

--- techmeet/interface/api/serializers.py
+++ techmeet/interface/api/serializers.py
@@ -1,9 +1,12 @@
 """API Serializers."""
-from django.contrib.auth.models import Group, User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 from rest_framework import serializers
 
 from data import models
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -11,7 +14,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:  # noqa: D106
         model = User
-        fields = ("url", "username", "email", "groups")
+        fields = ("url", "email", "name", "date_joined", "groups")
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
