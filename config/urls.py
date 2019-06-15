adc299d737a9bf3a7d8d5d6362413de2253fab9c
Ashia <mail@ashia.ca>
2019-06-15 14:11:13 -0600

    remove unused import

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views import defaults as default_views
 
 from rest_framework import routers
 
