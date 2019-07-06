9521c2ec6225e46a4d7cd64f8316415fcc358e0b
Ashia <mail@ashia.ca>
2019-07-06 13:34:18 -0600

    Register event model in admin

--- techmeet/data/admin.py
+++ techmeet/data/admin.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.User)
-admin.site.register(models.TechGroup)
+admin.site.register(models.Event)
 admin.site.register(models.Membership)
+admin.site.register(models.TechGroup)
+admin.site.register(models.User)
